a384659f0b11059890afec1a19723cdb6fa16d7f
Ali Elsaadany <alielsaadany94@gmail.com>
2024-03-29 02:12:32 +0000

    freeCodeCamp

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const bodyparser = require("body-parser")
+const dns = require("dns")
 
+const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(express.json())
 app.use(cors());
+
+let urls = [];
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', bodyparser.urlencoded({ extended: false }), async function (req, res) {
+try {
+  let { url } = req.body
+  let domain = await new URL(url).hostname;
+   await dns.lookup(domain, (err) => {
+    console.log(err);
+    if (err) {
+      return res.json({ error: "invalid url" })
+    
+    }else{
+      urls.push({ original_url: url, short_url: urls.length })
+      return res.json({ original_url: url, short_url: urls.length });  
+    }
+  });
+
+} catch (error) {
+  return res.json({ error: error.message });
+}
+
+});
+
+app.get('/api/shorturl/:short_url', function (req, res) {
+  let {short_url} = req.params;
+  res.redirect(short_url);
+});
+
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
